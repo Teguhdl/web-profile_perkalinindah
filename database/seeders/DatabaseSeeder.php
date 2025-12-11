af6de1fa83233cca9be1075fe24bec6cbffd1385
Teguh Bangun <teguhdianbangun@gmail.com>
2025-12-11 13:42:44 +0700

    seede

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PagesSeeder::class,
+            ProductSeeder::class,
+            PortfolioSeeder::class,
         ]);
     }
 }
