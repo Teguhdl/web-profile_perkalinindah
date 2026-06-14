15f4154220b46d1fad6d3313f6ca44b74d48904e
Teguh <teguhdianbangun@gmail.com>
2026-06-14 15:53:16 +0700

    google site

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -1,5 +1,6 @@
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="google-site-verification" content="UwRsNHd2AjSapOAnKySlIC6GzVWWcS2jpfEwyYH4xyc" />
  
     <!-- Favicon / Logo Browser -->
     <link rel="icon" type="image/png" href="{{ asset($settings['system_logo'] ?? 'assets/web/logo/logo.png') }}">
